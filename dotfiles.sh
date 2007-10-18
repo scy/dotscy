c182f242236e4e6a9a75c7291dd280d1464cdcc1
Tim Weber <scy-git@scytale.name>
2007-10-18 14:08:35 +0200

    Created dotfiles.sh and added .Xdefaults for xterm as well as .Xresources->.Xdefaults.

---
+++ dotfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+TPLDIR="$HOME/.$USER"
+LNBASE=".$USER"
+OUTDIR="$HOME"
+SEDCMD="s/^(\.git|dotfiles.sh)$//"
+
+die() {
+	echo dotfiles.sh: "$1" >&2
+	exit 1
+}
+
+cd "$TPLDIR" || die "could not change to template directory '$TPLDIR'"
+
+find . -maxdepth 1 | grep -v '^\.$' | cut -d / -f 2- | sed -r -e "$SEDCMD" | while read ITEM; do
+	if [[ ! -z "$ITEM" ]]; then
+		LNTARGET="$LNBASE/$ITEM"
+		if [[ -L "$ITEM" ]]; then
+			# If template is symlink, copy instead of linking to link.
+			cp -uPa "$ITEM" "$OUTDIR"
+		elif [[ ( ! -L "$OUTDIR/$ITEM" ) || ( "$(stat -c %Y "$OUTDIR/$ITEM" 2>/dev/null)" -lt "$(stat -c %Y "$ITEM")" ) ]]; then
+			# If target doesn't exist as symlink or is out-of-date, link it.
+			ln -sf "$LNTARGET" "$OUTDIR/$ITEM"
+		fi
+	fi
+done
