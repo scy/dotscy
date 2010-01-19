80d7dfd84d48614aca0e78eeb323259fd95aee43
Tim Weber <scy@scytale.name>
2010-01-19 12:05:59 +0100

    Automatically configure external monitors.

---
+++ bin/autovga.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if xrandr | grep -q '^VGA1 connected '; then
+	if lsusb | grep -q ' ID 046a:0004 '; then
+		# Docked.
+		xrandr --output VGA1 --mode 1280x1024 --pos 1024x0 --output LVDS1 --pos 0x460
+	else
+		# Undocked with external display (projector etc).
+		xrandr --output VGA1 --mode 1024x768 --pos 1024x0 --output LVDS1 --pos 0x0
+	fi
+else
+	# No external display. Currently, do nothing.
+	:
+fi
