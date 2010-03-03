9fde58d43596d3695d769a55591b6bcbb36c67b9
Tim Weber <scy@scytale.name>
2010-03-03 09:33:51 +0100

    autovga.sh: Configure dual-screen work machine.

--- bin/autovga.sh
+++ bin/autovga.sh
@@ -8,6 +8,9 @@ if xrandr | grep -q '^VGA1 connected '; then
 		# Undocked with external display (projector etc).
 		xrandr --output VGA1 --mode 1024x768 --pos 1024x0 --output LVDS1 --pos 0x0
 	fi
+elif xrandr | grep -q '^DVI-0 connected '; then
+	# Work machine.
+	xrandr --output DVI-0 --right-of VGA-0
 else
 	# No external display. Currently, do nothing.
 	:
