04e256118656dfe057385142f36f477ec8b935f2
Tim Weber <scy@scytale.name>
2012-11-02 15:07:41 +0100

    Set scroll direction in x-peripherals.sh
    x-peripherals.sh once was autovga.sh, but now does more than just
    display modes, hence the rename.
    
    The idea is that x-peripherals.sh can be run whenever the device setup
    changes while X is running, for example when (un)docking or plugging in
    a new mouse.

--- bin/x-peripherals.sh
+++ bin/x-peripherals.sh
@@ -7,3 +7,13 @@ if [ "$(hostname)" = 'coco-nb-434' ]; then      # Work machine.
 		xrandr --output LVDS1 --auto --primary --output HDMI3 --off
 	fi
 fi # If not work machine, do nothing.
+
+# If we have xinput, swap the scroll direction. (I'm OS X influenced.)
+if which xinput >/dev/null 2>&1; then
+	for dev in \
+	'Logitech USB Optical Mouse' \
+	'SynPS/2 Synaptics TouchPad' \
+	; do
+		xinput set-button-map "$dev" 1 2 3 5 4 6 7
+	done
+fi
