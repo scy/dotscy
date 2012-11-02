1847171ecbe4b45a8d96b3bcdac1634614fbc0de
Tim Weber <scy@scytale.name>
2012-11-02 14:57:02 +0100

    Modify autovga.sh for the setup at Cocomore

--- bin/autovga.sh
+++ bin/autovga.sh
@@ -1,17 +1,9 @@
 #!/bin/sh
 
-if xrandr | grep -q '^VGA1 connected '; then
-	if lsusb | grep -q ' ID 046a:0004 '; then
-		# Docked.
-		xrandr --output VGA1 --mode 1280x1024 --pos 1024x0 --output LVDS1 --pos 0x460
-	else
-		# Undocked with external display (projector etc).
-		xrandr --output VGA1 --mode 1024x768 --pos 1024x0 --output LVDS1 --pos 0x0
+if [ "$(hostname)" = 'coco-nb-434' ]; then      # Work machine.
+	if lsusb | grep -q ' ID 17ef:100a '; then   # Docked.
+		xrandr --output LVDS1 --auto --primary --pos 0x300 --output HDMI3 --auto --pos 1600x0
+	else                                        # Not docked.
+		xrandr --output LVDS1 --auto --primary --output HDMI3 --off
 	fi
-elif xrandr | grep -q '^DVI-0 connected '; then
-	# Work machine.
-	xrandr --output DVI-0 --left-of VGA-0
-else
-	# No external display. Currently, do nothing.
-	:
-fi
+fi # If not work machine, do nothing.
