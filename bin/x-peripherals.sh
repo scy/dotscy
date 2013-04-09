daee2f6d06c17e577180a858aea324df5bdaa4dd
Tim Weber <scy@scytale.name>
2013-04-09 16:19:17 +0200

    xrandr at work: Align bottom edges
    This (hopefully) will fix some notification popup problems I'm currently
    having.
    
    See:
    https://faq.i3wm.org/question/960/dual-screen-skype-notifications-appear-off-screen/

--- bin/x-peripherals.sh
+++ bin/x-peripherals.sh
@@ -2,7 +2,7 @@
 
 if [ "$(hostname)" = 'coco-nb-434' ]; then      # Work machine.
 	if lsusb | grep -q ' ID 17ef:100a '; then   # Docked.
-		xrandr --output LVDS1 --auto --primary --pos 0x300 --output HDMI3 --auto --pos 1600x0
+		xrandr --output LVDS1 --auto --primary --pos 0x180 --output HDMI3 --auto --pos 1600x0
 	else                                        # Not docked.
 		xrandr --output LVDS1 --auto --primary --output HDMI3 --off
 	fi
