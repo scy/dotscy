858e767379f3a040f95dc3fe0a6d0efb4ab8d3bf
Tim Weber <scy@scy.name>
2013-07-24 16:53:45 +0200

    Make have_xinput a function

--- bin/x-peripherals.sh
+++ bin/x-peripherals.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Checks whether we have xinput.
+have_xinput () {
+	which xinput >/dev/null 2>&1
+}
+
 
 
 # Screen setup, based on which machine this is and whether it's docked.
@@ -16,7 +21,7 @@ fi # If not work machine, do nothing.
 
 # If we have xinput, swap the mouse scroll direction. (I'm OS X influenced.)
 
-if which xinput >/dev/null 2>&1; then
+if have_xinput; then
 	xinput list --name-only | grep -F "$(printf "\
 Logitech USB Optical Mouse\n\
 SynPS/2 Synaptics TouchPad\
